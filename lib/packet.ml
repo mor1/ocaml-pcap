4bc57c0fc78bad269816550e68cd744eb90f7a6c
Richard Mortier <mort@cantab.net>
2013-09-15 23:47:34 +0100

    [formats] rename IP/TCP/UDP to xxx4

--- lib/packet.ml
+++ lib/packet.ml
@@ -17,13 +17,13 @@
 open Printf
   
 let buf_to_string pfx buf = 
-  let line_len = 32 in
+  let line_len = 16 in
   
   let line bs = 
     let line = ref "" in
     for i = 0 to (min line_len (Cstruct.len bs)) - 1do
-      if i > 0 && i mod 8 == 0 then line := !line ^ ".";
-      line := sprintf "%s%02x" !line (Cstruct.get_uint8 bs i)
+      if i > 0 && i mod 8 == 0 then line := !line ^ " ";
+      line := sprintf "%s%02x." !line (Cstruct.get_uint8 bs i)
     done;
     !line
   in
@@ -42,9 +42,9 @@ let buf_to_string pfx buf =
 
 type t = 
   | ETH  of Ethernet.h * t
-  | IP   of Ip.h * t
-  | TCP  of Tcp.h * t
-  | UDP  of Udp.h * t
+  | IP4 of Ip4.h * t
+  | TCP4  of Tcp4.h * t
+  | UDP4  of Udp4.h * t
       
   | DHCP of Dhcp.t
 
@@ -56,12 +56,12 @@ let to_str pkt =
     match pkt with
       | ETH (h, p) -> 
         let s = sprintf "%s|ETH(%s)" str (Ethernet.to_str h) in aux p s
-      | IP (h, p)   -> 
-        let s = sprintf "%s|IP(%s)" str (Ip.to_str h) in aux p s
-      | TCP (h, p)  -> 
-        let s = sprintf "%s|TCP(%s)" str (Tcp.to_str h) in aux p s
-      | UDP (h, p)  -> 
-        let s = sprintf "%s|UDP(%s)" str (Udp.to_str h) in aux p s
+      | IP4 (h, p)   -> 
+        let s = sprintf "%s|IP4(%s)" str (Ip4.to_str h) in aux p s
+      | TCP4 (h, p)  -> 
+        let s = sprintf "%s|TCP4(%s)" str (Tcp4.to_str h) in aux p s
+      | UDP4 (h, p)  -> 
+        let s = sprintf "%s|UDP4(%s)" str (Udp4.to_str h) in aux p s
 
       | DHCP p -> sprintf "%s|%s" str (Dhcp.to_str p)
 
@@ -75,12 +75,12 @@ let to_string pkt =
     match pkt with
       | ETH (h, p) -> 
         let s = sprintf "%s|ETH(%s)" str (Ethernet.to_string h) in aux p s
-      | IP (h, p)   -> 
-        let s = sprintf "%s|IP(%s)" str (Ip.to_string h) in aux p s
-      | TCP (h, p)  -> 
-        let s = sprintf "%s|TCP(%s)" str (Tcp.to_string h) in aux p s
-      | UDP (h, p)  -> 
-        let s = sprintf "%s|UDP(%s)" str (Udp.to_string h) in aux p s
+      | IP4 (h, p)   -> 
+        let s = sprintf "%s|IP4(%s)" str (Ip4.to_string h) in aux p s
+      | TCP4 (h, p)  -> 
+        let s = sprintf "%s|TCP4(%s)" str (Tcp4.to_string h) in aux p s
+      | UDP4 (h, p)  -> 
+        let s = sprintf "%s|UDP4(%s)" str (Udp4.to_string h) in aux p s
 
       | DHCP p -> sprintf "%s|%s" str (Dhcp.to_string p)
 
@@ -88,3 +88,4 @@ let to_string pkt =
       | ERROR bs -> sprintf "%s|ERR(%s)" str (buf_to_string "\t" bs)
   in
   aux pkt ""
+ 
