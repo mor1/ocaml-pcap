691e93effe6dc3b3bed83bf05af1d901b99d09a0
Richard Mortier <mort@cantab.net>
2013-09-19 20:50:36 +0100

    [flows] refactor to modules, distinguishing uni- and bi-directional flows

--- flows/flows.ml
+++ flows/flows.ml
@@ -19,98 +19,195 @@ open Pcap
 open Printf
 open Packet
 
-(** flow as IP 4-tuple (since we only care about TCP) *)
-type flow = (int32 * int * int32 * int)
+module Unidir = struct
 
-let flow ih th = (ih.Ip4.src, th.Tcp4.srcpt, ih.Ip4.dst, th.Tcp4.dstpt)
-let flow_to_string (srcip, srcpt, dstip, dstpt) = 
-  sprintf "%s/%d -> %s/%d"
-    (Ip4.ip_to_string srcip) srcpt (Ip4.ip_to_string dstip) dstpt
-
-(** Flow table *)
-module Flow = struct
-  type t = flow
+  type t = (int32 * int * int32 * int)
+    
   let compare x y = compare x y
+
+  let t ih th = 
+    (ih.Ip4.src, th.Tcp4.srcpt, ih.Ip4.dst, th.Tcp4.dstpt)
+
+  let to_string (tx, txpt, rx, rxpt) =
+    sprintf "%s/%d -> %s/%d"
+      (Ip4.ip_to_string tx) txpt (Ip4.ip_to_string rx) rxpt
+
 end
 
-module Flows = Map.Make(Flow)
+module Bidir = struct
 
-(** State record: number of flows, and packets-per-flow *)
-type st = {
-  mutable nflows: int;
-  mutable flows: int Flows.t;
-}
-let st = {
-  nflows = 0;
-  flows = Flows.empty;
-}
+  type t = (int32 * int * int32 * int)
+  type dir = OUT | BACK
+  type f = t * dir
+  
+  let compare x y = compare x y
 
-(* filter and process only TCP/IP packets to count 4-tuple flows *)
-let flow_counter acc = function
-  | PCAP(h, ETH(eh, IP4(ih, TCP4(th, _)))) ->
-    let f = flow ih th in
-    (try
-      let n = Flows.find f st.flows in
-      st.flows <- Flows.add f (n+1) st.flows;
-    with Not_found -> 
-      st.flows <- Flows.add f 1 st.flows;
-      st.nflows <- st.nflows + 1;
-    );
-    acc+1
-  | _ -> acc+1
+  let t ih th = (ih.Ip4.src, th.Tcp4.srcpt, ih.Ip4.dst, th.Tcp4.dstpt)
+  
+  let f ih th = 
+    let (tx, txpt, rx, rxpt) = 
+      (ih.Ip4.src, th.Tcp4.srcpt, ih.Ip4.dst, th.Tcp4.dstpt)
+    in
+    if (tx < rx) || (tx == rx && txpt <= rxpt) then 
+      (tx, txpt, rx, rxpt, OUT) 
+    else
+      (rx, rxpt, tx, txpt, BACK)
 
-(* customise protocol demux to be as shallow as we can: ETH -> IP -> TCP ->
-   DROP; not strictly necessary as it's hardly likely that doing a full demux
-   (per Demux.eth_demux) is a bottleneck given how few protocols are implemented,
-   but an excuse to test in principle *)
-let flow_demux st buf = 
-  let shallow_ipproto_demux st ih = 
-    let open Ip4 in
-    match int_to_protocol ih.proto with
-      | Some t -> (match t with
-          | TCP -> Demux.tcp4_demux st (fun st th -> Demux.drop_demux)
-          | _ -> Demux.drop_demux
-      )
-      | None -> Demux.drop_demux
+  let f_to_t (tx, txpt, rx, rxpt, dir) =
+    match dir with
+      | OUT -> tx, txpt, rx, rxpt
+      | BACK -> rx, rxpt, tx, txpt
+
+  let to_string (tx, txpt, rx, rxpt) =
+    sprintf "%s/%d -> %s/%d"
+      (Ip4.ip_to_string tx) txpt (Ip4.ip_to_string rx) rxpt
+
+  let f_to_string (tx, txpt, rx, rxpt, dir) =
+    let tx, txpt, rx, rxpt = match dir with
+        | OUT -> ((Ip4.ip_to_string tx), txpt, (Ip4.ip_to_string rx), rxpt)
+        | BACK -> ((Ip4.ip_to_string rx), rxpt, (Ip4.ip_to_string tx), txpt)
+    in
+    sprintf "%s/%d -> %s/%d" tx txpt rx rxpt
+end
+
+module UniFlows = Map.Make(Unidir)  
+module BiFlows = Map.Make(Bidir)
+
+module Flowstate = struct
+  type t = {
+    mutable npkts: int;
+  }
+
+  let to_string t = 
+    sprintf "npkts:%d" t.npkts
+
+  let create f = 
+    { npkts = 0;
+    }
+    
+  let update fst f = 
+    { fst with npkts = fst.npkts + 1
+    }
+
+end
+
+module State = struct
+  type t = {
+    mutable npkts: int;
+    mutable nflows: int;
+    mutable biflows: Flowstate.t BiFlows.t;
+  }
+
+  let to_string t = 
+    let hdr = sprintf "npkts: %d\nnflows: %d\n" t.npkts t.nflows in
+    BiFlows.fold
+      (fun f fst acc -> 
+        sprintf "%s%s: %s\n" acc (Bidir.to_string f) (Flowstate.to_string fst)
+      ) 
+      t.biflows hdr
+  
+  let dump t = 
+    printf "npkts: %d\nnflows: %d\n" t.npkts t.nflows;
+    BiFlows.iter
+      (fun f fst -> 
+        printf "%s: %s\n" (Bidir.to_string f) (Flowstate.to_string fst)
+      ) 
+      t.biflows 
+end
+
+(** how to process each packet *)
+let pkt_process st pkt =
+  State.(
+    if st.npkts mod 1_000_000 == 0 then
+      printf "npkts=%d nflows=%d\n%!" st.npkts st.nflows
+  );
+
+  (match pkt with
+    | PCAP(h, ETH(eh, IP4(ih, TCP4(th, _)))) ->
+      let t = Bidir.t ih th in
+      let f = Bidir.f ih th in
+      State.(
+         let fst = 
+           try
+             BiFlows.find t st.biflows
+           with Not_found -> Flowstate.create f
+         in
+         st.biflows <- BiFlows.add t (Flowstate.update fst f) st.biflows;
+         st.nflows <- st.nflows + 1;
+      );
+    | _ -> ()
+  );
+  State.(st.npkts <- st.npkts + 1);
+  st
+
+(** parse a buffer, with state *)
+let parse st buf =
+  
+  (** customise protocol demux to be as shallow as we can: ETH -> IP -> TCP ->
+      DROP; not strictly necessary as it's hardly likely that doing a full demux
+      (per Demux.eth_demux) is a bottleneck given how few protocols are
+      implemented, but an excuse to test in principle *)
+  let flow_demux st buf = 
+    let shallow_ipproto_demux st ih = 
+      let open Ip4 in
+      match int_to_protocol ih.proto with
+        | Some t -> (match t with
+            | TCP -> Demux.tcp4_demux st (fun st th -> Demux.drop_demux)
+            | _ -> Demux.drop_demux
+        )
+        | None -> Demux.drop_demux
+    in
+    let shallow_ethertype_demux st eh = 
+      let open Ethernet in
+      match int_to_ethertype eh.ethertype with
+        | Some t -> (match t with
+            | IP4 -> Demux.ip_demux st shallow_ipproto_demux
+            | _ -> Demux.drop_demux
+        )
+        | None -> Demux.drop_demux
+    in
+    Demux.eth_demux st shallow_ethertype_demux buf
   in
-  let shallow_ethertype_demux st eh = 
-    let open Ethernet in
-    match int_to_ethertype eh.ethertype with
-      | Some t -> (match t with
-          | IP4 -> Demux.ip_demux st shallow_ipproto_demux
-          | _ -> Demux.drop_demux
-      )
-      | None -> Demux.drop_demux
-  in
-  Demux.eth_demux st shallow_ethertype_demux buf
 
-let filename_to_buf filename = 
-  printf "filename: %s\n" filename;
-  let fd = Unix.(openfile filename [O_RDONLY] 0) in
-  let buf = Bigarray.(Array1.map_file fd Bigarray.char c_layout false (-1)) in
-  let buf = Cstruct.of_bigarray buf in
-  printf "total pcap file length %d\n" (Cstruct.len buf);
-  buf
-
-let parse buf =
+  (* actually proces the buffer, folding over the packets it contains *)
   match Pcap.iter buf (flow_demux st) with
     | None -> 
       fprintf stderr "not a pcap file (failed to read magic number in header)\n%!"
         
-    | Some (pcap_header, pcap_packets) -> 
+    | Some (header, packets) -> 
       let open Pcap in
-      printf "### %s\n%!" (fh_to_string pcap_header);
-      let num_packets = Cstruct.fold flow_counter pcap_packets 0 in
-      printf "num_packets %d\n" num_packets;
-      printf "num_flows %d == %d\n%!" st.nflows (Flows.cardinal st.flows)
+      printf "### %s\n%!" (fh_to_string header);
+      State.(
+        let st = Cstruct.fold pkt_process packets st in
+        printf "npkts: %d\n" st.npkts;
+        printf "nflows: %d == %d\n%!" st.nflows (BiFlows.cardinal st.biflows)
+      )
 
+(** convert [filename] string to a buffer by opening and mapping file *)
+let filename_to_buf filename = 
+  let fd = Unix.(openfile filename [O_RDONLY] 0) in
+  let buf = Bigarray.(Array1.map_file fd Bigarray.char c_layout false (-1)) in
+  let buf = Cstruct.of_bigarray buf in
+  buf
+
+(** entry point *)
 let _ =
+  let st = State.({
+    npkts = 0;
+    nflows = 0;
+    biflows = BiFlows.empty;
+  })
+  in
   let files = ref [] in
   Arg.parse []
     (fun x -> files := x :: !files)
     "Dump the contents of pcap files";
   let files = List.rev !files in
-  List.iter (fun file -> file |> filename_to_buf |> parse) files;
+  List.iter (fun file -> file |> filename_to_buf |> parse st) files;
 
-  printf "flows:\n%!";
-  Flows.iter (fun f n -> printf "\t%s = %d\n%!" (flow_to_string f) n) st.flows
+  (* stupid idea. this many flows, all this does is try to build a string of
+     many hundreds of MB, and then print it. abstraction taken a step to far.
+
+     printf "%s\n%!" (State.to_string st) 
+  *)
+  State.dump st
